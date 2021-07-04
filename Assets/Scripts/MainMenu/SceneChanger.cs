210e0e06fe4009092eae0a9e27021281a2c04383
Noahsonicx <53206519+Noahsonicx@users.noreply.github.com>
2021-07-04 21:26:59 +1000

    Updated
    Cursor
    logo
    splash Screen
    Tried to Rework the pause menu and all the other menus but took too much time and i ran out of time

--- Assets/Scripts/MainMenu/SceneChanger.cs
+++ Assets/Scripts/MainMenu/SceneChanger.cs
@@ -9,26 +9,38 @@ public class SceneChanger : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
+    /*
     public void Options()
     {
         SceneManager.LoadScene("Options");
     }
+    
     public void PauseMenu()
     {
         FindObjectOfType<KeyBinding>().Save();
         SceneManager.LoadScene("PauseMenu");
     }
+    */
     public void GameScene()
     {
+        FindObjectOfType<KeyBinding>().Save();
         SceneManager.LoadScene("GameScene");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
     public void LoadGame()
     {
         SceneManager.LoadScene("LoadGame");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
     public void SaveGame()
     {
         SceneManager.LoadScene("SaveGame");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
